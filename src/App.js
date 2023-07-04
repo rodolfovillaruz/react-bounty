5e6339cd7d682d2b0320fc705fa1d8c71ead2d72
Rodolfo Villaruz <rodolfovillaruz@gmail.com>
2023-07-04 04:24:56 +0000

    Static header and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-screen">
+        <div className="App-container">
+          <div className="App-header">
+            <div className="App-header-links">
+              Bounty Logo
+            </div>
+            <div className="App-header-push"></div>
+            <div className="App-header-links">
+              Home
+            </div>
+            <div className="App-header-links">
+              |
+            </div>
+            <div className="App-header-links">
+              About Us
+            </div>
+          </div>
+          <div className="App-contents">
+            <h1>What we do</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum felis eu mi imperdiet, id dapibus orci hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam dolor odio, pretium vitae est at, fermentum ornare massa. Vivamus aliquam tristique libero vel efficitur. Nulla iaculis, diam vel auctor pretium, justo lorem pellentesque massa, vestibulum vulputate risus nibh vitae felis. Maecenas sed leo varius, malesuada eros id, ultrices lorem. Quisque auctor ligula eget nulla egestas elementum.
+            </p>
+          </div>
+          <div className="App-footer">
+            <div className="App-header-links">
+              © 2022 Bounty Media Pte. Ltd, Singapore.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
